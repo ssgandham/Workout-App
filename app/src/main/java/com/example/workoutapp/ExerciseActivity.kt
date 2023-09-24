655f6d53f2deb2c685ec2163d873ea8787721cdb
ssgandham <gandhamsnehalsrinivas@gmail.com>
2023-09-24 09:44:43 -0400

    Test again

--- app/src/main/java/com/example/workoutapp/ExerciseActivity.kt
+++ app/src/main/java/com/example/workoutapp/ExerciseActivity.kt
@@ -1,20 +1,26 @@
 package com.example.workoutapp
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.workoutapp.databinding.ActivityExerciseBinding
-import com.example.workoutapp.databinding.ActivityMainBinding
 
 class ExerciseActivity : AppCompatActivity() {
-    private var binding: ActivityExerciseBinding?= null
-
+    //Todo 1 creat a binding variable
+    private var binding:ActivityExerciseBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_exercise)
-
+//Todo 2 inflate the layout
         binding = ActivityExerciseBinding.inflate(layoutInflater)
+//Todo 3 pass in binding?.root in the content view
         setContentView(binding?.root)
+//Todo 4: then set support action bar and get toolBarExcerciser using the binding
+//variable
+        setSupportActionBar(binding?.toolbarExercise)
 
-
+        if (supportActionBar != null){
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+        binding?.toolbarExercise?.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
