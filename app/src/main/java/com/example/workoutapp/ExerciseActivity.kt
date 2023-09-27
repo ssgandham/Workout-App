3c298ea30e9c32ca8ee1eebf06071edcad7a4d87
ssgandham <gandhamsnehalsrinivas@gmail.com>
2023-09-26 20:18:38 -0400

    Timer Feature

--- app/src/main/java/com/example/workoutapp/ExerciseActivity.kt
+++ app/src/main/java/com/example/workoutapp/ExerciseActivity.kt
@@ -1,19 +1,46 @@
 package com.example.workoutapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import com.example.workoutapp.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
-    //Todo 1 creat a binding variable
     private var binding:ActivityExerciseBinding? = null
+    private var timer: CountDownTimer? = null
+    private var progressSoFar = 0
+
+    private fun setProgressBar(){
+        binding?.progressBar?.progress = progressSoFar
+
+        timer = object: CountDownTimer(5000, 1000){
+            override fun onTick(p0: Long) {
+                progressSoFar++
+                binding?.progressBar?.progress = 5 - progressSoFar
+                binding?.timer?.text = (5 - progressSoFar).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(
+                    this@ExerciseActivity,
+                    "Here now we will start the exercise.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }.start()
+    }
+    private fun setupRestView(){
+        if(timer!=null){
+            timer!!.cancel()
+            progressSoFar = 0
+        }
+
+        setProgressBar()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//Todo 2 inflate the layout
         binding = ActivityExerciseBinding.inflate(layoutInflater)
-//Todo 3 pass in binding?.root in the content view
         setContentView(binding?.root)
-//Todo 4: then set support action bar and get toolBarExcerciser using the binding
-//variable
         setSupportActionBar(binding?.toolbarExercise)
 
         if (supportActionBar != null){
@@ -22,5 +49,16 @@ class ExerciseActivity : AppCompatActivity() {
         binding?.toolbarExercise?.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        setupRestView()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (timer != null) {
+            timer?.cancel()
+            progressSoFar = 0
+        }
+        binding = null
     }
 }
